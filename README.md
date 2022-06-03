dd862d014c232563891436881d8f94674ec4a7d1
hamiat <hamiat.n@hotmail.com>
2022-06-03 11:23:57 +0200

    api initialized

---
+++ README.md
@@ -0,0 +1,6 @@
+This project (api) was created with the MERN stack. 
+Client - React 
+Server - Express
+Postman - debugging
+MongoDB - Database
+CURD (post, get, put, delete)
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-console.log('hey')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const port = process.env.PORT || 5000
+
+const app = express()
+
+app.get('/api/board', (req, res) => {
+    res.status(200).json({message: 'Get Board'})
+})
+
+app.listen(port, () => console.log(`Server started on port ${port}.`))
